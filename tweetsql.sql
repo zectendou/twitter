d14dfed85f7d7822705fa702ae7d55b5524db1ed
m-stake <LFAFI@m-stake-no-MacBook-Pro.local>
2020-01-22 20:11:29 +0900

    first commit

---
+++ tweetsql.sql
@@ -0,0 +1,15 @@
+create table tweet_search(
+    tweet_id int, ---parse from id_str---
+    name_id varchar(20), ---screen_name---
+    tweet_contents varchar(150), ---text---
+    tweet_profile varchar(200), ---desctription---
+    follower int,---followers_count---
+    tweet_day date, ---parse from created_at---
+    tweet_time time,---parse from created_at---
+    link boolean,---parse from url---
+    media int,---parse from media---
+    favorite_count int,---favorite_count---
+    quoted boolean,---parse from is_quate_status---
+    retweet int,---parse from retweet_count---
+    mention boolean,---parse from usermention---
+);
