82d11c8b81d402130d1437bdc2ffee4c30ccc198
zectendou <msatake1218@gmail.com>
2020-02-12 09:51:50 +0900

    parse from twitter

---
+++ json.py
@@ -0,0 +1,54 @@
+import json
+import os
+from datetime import datetime
+
+max_id = -1
+
+while(True):
+    if max_id != -1:
+        params['max_id'] = max_id -1
+        req = twitter.get(url, params = params)
+
+        if req.status_code ==200:
+            tweets = json.loads(req.text)
+        
+            if tweets['statuses'] ==[]:
+                break
+                
+
+
+
+            for tweet in tweets["statuses"]:
+                tweet_id = int(tweet["id_str"])
+                name_id = tweet["screenname"]
+                twitter_name = tweet["name"]
+                tweet_contents  = tweet["full_text"]
+                tweet_profile = tweet["description"]
+                follower = tweet[""]
+                tweettime = datetime.strptime(tweet["created_at"], '%a %b %d %H:%M:%S %z %Y')
+                tweet_datetime = tweettime.datetime('%Y, %m, %d, %H,%M,%S')))
+                if tweets.get("extended_entities"]:
+                    media_list = tweet['extended_entities']["media"]
+                    for media in media_list:
+                        media_type = media_list["type"]
+                        media_url = media_type["expanded_url"]
+                if tweets.get["extended_entities"]:
+                    url_list = tweet["extended_entities"]
+                    for  url in url_list:
+                        link = url_list["url"]
+                favorite_count = tweet["favoriet_count"]
+                quoted = tweet["is_quate_status"]
+                retweet = tweet["retweet_count"]
+                mention = tweet[]"usermention"]
+
+
+            max_id = tweets['statuses'][-1]['id']
+        
+        else:
+            print('15分待ってね！')
+            sleep(15*60)
+
+
+
+    
+    
